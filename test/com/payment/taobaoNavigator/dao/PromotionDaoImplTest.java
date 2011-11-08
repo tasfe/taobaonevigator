bf7667c99ee8ed2d0c81bf62ce94b40a18e1bef7
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-08 09:29:43 +0000

    initialize test cases for Promotion module.

---
+++ test/com/payment/taobaoNavigator/dao/PromotionDaoImplTest.java
@@ -0,0 +1,74 @@
+package com.payment.taobaoNavigator.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PromotionDaoImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetPromotions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEnabledPromotions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPromotionById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPromotionByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreatePromotion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePromotion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePromotionById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePromotionByIds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePromotionByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePromotionByNames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearPromotions() {
+		fail("Not yet implemented");
+	}
+
+}
