ccd51b41900141b34d8e2519677d0b6333517f0a
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-08 10:05:49 +0000

    delete demo

--- trunk/src/com/payment/taobaoNavigator/controller/NavigatorController.java
+++ trunk/src/com/payment/taobaoNavigator/controller/NavigatorController.java
@@ -2,28 +2,20 @@ package com.payment.taobaoNavigator.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.payment.taobaoNavigator.form.DemoForm;
-import com.payment.taobaoNavigator.service.NavigatorService;
 import com.payment.taobaoNavigator.util.NavigatorCache;
 
 @Controller
 public class NavigatorController {
-	
+
 	@RequestMapping("index.do")
 	public ModelAndView index() {
-		
-	List list=NavigatorCache.processAction("query",0);
-	System.out.println(list.size()+"!!!!!!");
-	DemoForm demoForm =new DemoForm();
-	
-	
-		return new ModelAndView("demo", "demoForm", demoForm);
+
+		List list = NavigatorCache.processAction("query", 0);
+		System.out.println(list.size() + "!!!!!!");
+		return null;
 	}
 }
