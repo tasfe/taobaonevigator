44bd8d6fdd71135c9750205964137a1339a5e71e
rainbowof@gmail.com <rainbowof@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-09 09:28:10 +0000

    most of the impl of the promotiondao

--- src/com/payment/taobaoNavigator/dao/PromotionDaoImpl.java
+++ src/com/payment/taobaoNavigator/dao/PromotionDaoImpl.java
@@ -22,62 +22,97 @@ public class PromotionDaoImpl extends BaseDaoImpl implements PromotionDao {
 
 	@Override
 	public List<PromotionEntity> getEnabledPromotions() {
-
-		return null;
+		String hql = "select p from PromotionEntity p where p.enabled=1";
+		Query query = em.createQuery(hql);
+		List<PromotionEntity> promotions = query.getResultList();
+		return promotions;
 	}
 
 	@Override
 	public PromotionEntity getPromotionById(Integer id) {
-
-		return null;
+		String hql = "select p from PromotionEntity p where p.id='" + id + "'";
+		Query query = em.createQuery(hql);
+		PromotionEntity promotionEntity = (PromotionEntity) query
+				.getSingleResult();
+		return promotionEntity;
 	}
 
 	@Override
 	public PromotionEntity getPromotionByName(String name) {
-
-		return null;
+		String hql = "select p from PromotionEntity p where p.name='" + name
+				+ "'";
+		Query query = em.createQuery(hql);
+		PromotionEntity promotionEntity = (PromotionEntity) query
+				.getSingleResult();
+		return promotionEntity;
 	}
 
 	@Override
+	@Transactional
 	public PromotionEntity createPromotion(PromotionEntity promotion) {
-
+		create(promotion);
 		return null;
 	}
 
 	@Override
 	public PromotionEntity updatePromotion(PromotionEntity promotion) {
-
+		em.merge(promotion);
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public boolean deletePromotionById(Integer id) {
-
-		return false;
+		boolean flag = false;
+		PromotionEntity promotionEntity = em.find(PromotionEntity.class, id);
+		em.remove(promotionEntity);
+		flag = true;
+		return flag;
 	}
 
 	@Override
+	@Transactional
 	public boolean deletePromotionByIds(List<Integer> ids) {
-
-		return false;
+		boolean flag = false;
+		for (Integer integer : ids) {
+			PromotionEntity promotionEntity = em.find(PromotionEntity.class,
+					integer);
+			em.remove(promotionEntity);
+		}
+		flag = true;
+		return flag;
 	}
 
 	@Override
+	@Transactional
 	public boolean deletePromotionByName(String name) {
-
-		return false;
+		boolean flag = false;
+		PromotionEntity promotionEntity = this.getPromotionByName(name);
+		em.remove(promotionEntity);
+		flag = true;
+		return flag;
 	}
 
 	@Override
+	@Transactional
 	public boolean deletePromotionByNames(List<String> names) {
-
-		return false;
+		boolean flag = false;
+		for (String name : names) {
+			this.deletePromotionByName(name);
+		}
+		flag = true;
+		return flag;
 	}
 
 	@Override
+	@Transactional
 	public boolean clearPromotions() {
-
-		return false;
+		boolean flag = false;
+		String hql = "delete from PromotionEntity p";
+		Query query = em.createQuery(hql);
+		query.executeUpdate();
+		flag = true;
+		return flag;
 	}
 
 }
