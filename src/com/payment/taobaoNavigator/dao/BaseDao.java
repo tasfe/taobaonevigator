add5e1f447a9d71d5f21863e4d9b2cdac62600c0
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-09 10:10:04 +0000

    1, when deleting a entity, no need to return the deleted entity 2, adjust the order of find()'s parameters with the same sort of em's method.

--- src/com/payment/taobaoNavigator/dao/BaseDao.java
+++ src/com/payment/taobaoNavigator/dao/BaseDao.java
@@ -8,8 +8,8 @@ import com.payment.taobaoNavigator.entity.BaseEntity;
 public interface BaseDao {
 	public <T extends BaseEntity> T create(T entity);
 	public <T extends BaseEntity> T update(T entity);
-	public <T extends BaseEntity> T delete(T entity);
-	public <T extends BaseEntity> T find(Object entityPk, Class<T> entityClass);
+	public <T extends BaseEntity> void delete(T entity);
+	public <T extends BaseEntity> T find(Class<T> entityClass, Object entityPk);
 	public <T extends BaseEntity> List<T> findByNamedQuery(Class<T> entityClass, String namedQuery);
 	public <T extends BaseEntity> List<T> findByNamedQuery(Class<T> entityClass, String namedQuery,
 			Map<String, Object> parameters);
--- src/com/payment/taobaoNavigator/dao/BaseDaoImpl.java
+++ src/com/payment/taobaoNavigator/dao/BaseDaoImpl.java
@@ -28,6 +28,7 @@ public abstract class BaseDaoImpl implements BaseDao{
 		em.persist(entity);
 		em.flush();
 		em.refresh(entity);
+		
 		return entity;
 	}
 	
@@ -39,15 +40,13 @@ public abstract class BaseDaoImpl implements BaseDao{
 
 	@Override
 	@Transactional
-	public <T extends BaseEntity> T delete(T entity) {
-		
+	public <T extends BaseEntity> void delete(T entity) {
 		 em.remove(entity);
-		 return entity; 
 	}
 
 	@Override
 	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
-	public <T extends BaseEntity> T find(Object entityPk, Class<T> entityClass) {
+	public <T extends BaseEntity> T find(Class<T> entityClass, Object entityPk) {
 
 		return (T) this.em.find(entityClass, entityPk);
 	}
@@ -56,22 +55,26 @@ public abstract class BaseDaoImpl implements BaseDao{
 	@Override
 	public <T extends BaseEntity> List<T> findByNamedQuery(
 			Class<T> entityClass, String namedQuery) {
+		
 		return findByNamedQuery(entityClass, namedQuery, null);
 	}
 
 	@Override
 	public <T extends BaseEntity> List<T> findByNamedQuery(
-			Class<T> entityClass, String namedQuery,
+			Class<T> entityClass, String queryName,
 			Map<String, Object> parameters) {
-		Query query=em.createNamedQuery(entityClass.getSimpleName()+"."+namedQuery);
+		Query query = em.createNamedQuery(entityClass.getSimpleName() + "." + queryName);
 		if (parameters != null) {
 			for (Entry<String, Object> param : parameters.entrySet()) {
 				query.setParameter(param.getKey(), param.getValue());
 			}
 		}
 		final List<T> resultList = query.getResultList();
-		if (resultList == null)
+		
+		if (resultList == null) {
 			return new LinkedList<T>();
+		}
+
 		return resultList;
 	}
 }
