d1e665ae5d2a6ff88f99b5cd61df28793f695061
feng.johnson.li@gmail.com <feng.johnson.li@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-09 07:20:26 +0000

    the catagory data script

---
+++ database/scripts/insert.sql
@@ -0,0 +1,5 @@
+insert into catagory vlues(0,"ROOT",0,"test0");
+insert into catagory vlues(1,"1",0,"test1");
+insert into catagory vlues(2,"2",0,"test2");
+insert into catagory vlues(3,"1.1",1,"test1.1");
+insert into catagory vlues(4,"1.2",1,"test1.2");
