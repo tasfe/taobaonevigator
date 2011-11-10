fa3eee022cebeb31604445963f280af5d5fb1f3b
feng.johnson.li@gmail.com <feng.johnson.li@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-10 02:16:09 +0000

    remove the root node as child list

--- trunk/src/com/payment/taobaoNavigator/util/NavigatorCache.java
+++ trunk/src/com/payment/taobaoNavigator/util/NavigatorCache.java
@@ -1,7 +1,7 @@
 package com.payment.taobaoNavigator.util;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +18,7 @@ public abstract class NavigatorCache {
 		ApplicationContext ac = new FileSystemXmlApplicationContext("classpath:applicationContext.xml");
 
 		navigatorServiceImpl = (NavigatorService) ac.getBean("navigatorServiceImpl");
-		System.out.println("init.....");
+		
 	}
 	private static Map<String, List<CatagoryEntity>> NavigatorCache = new HashMap<String, List<CatagoryEntity>>();
 
@@ -42,8 +42,21 @@ public abstract class NavigatorCache {
 		List<CatagoryEntity> childList = contain(parentId);
 		if (null == childList || 0 == childList.size()) {
 			childList = navigatorServiceImpl.findCatagoryByPid(parentId);
+			//remove the root get itself...
+//			java.util.ConcurrentModificationException
+			if(0==parentId){
+			 for (Iterator it = childList.iterator(); it.hasNext();) {
+				 CatagoryEntity child=(CatagoryEntity)it.next();
+				 if(child.getId()==0){
+						it.remove();
+					}
+				 
+				 System.out.println(child.getId()+"==="+child.getName());
+				 
+			 }
+			}
 			addNavigatorCache(parentId, childList);
-			System.out.println("put in the cache");
+			
 		}
 		return childList;
 	}
@@ -81,77 +94,78 @@ public abstract class NavigatorCache {
 	 * @return
 	 */
 	public static List<CatagoryEntity> getByParentId(int parentId) {
-		if (parentId == 0) {
-			List<CatagoryEntity> result = new ArrayList<CatagoryEntity>();
-			CatagoryEntity entity = createDefaultCatagoryEntity();
-			entity.setName("测试种类一");
-			entity.setId(1);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("测试种类二");
-			entity.setId(2);
-			result.add(entity);
-			System.out.println(result);
-			return result;
-		}
-		if (parentId == 1) {
-			List<CatagoryEntity> result = new ArrayList<CatagoryEntity>();
-			CatagoryEntity entity = createDefaultCatagoryEntity();
-			entity.setName("类一分类一");
-			entity.setId(3);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类一分类二");
-			entity.setId(4);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类一分类三");
-			entity.setId(5);
-			result.add(entity);
-			System.out.println(result);
-			return result;
-		}
-		if (parentId == 2) {
-			List<CatagoryEntity> result = new ArrayList<CatagoryEntity>();
-			CatagoryEntity entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类一");
-			entity.setId(6);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类二");
-			entity.setId(7);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类三");
-			entity.setId(8);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类4");
-			entity.setId(8);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类5");
-			entity.setId(8);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类6");
-			entity.setId(8);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类7");
-			entity.setId(8);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类8");
-			entity.setId(8);
-			result.add(entity);
-			entity = createDefaultCatagoryEntity();
-			entity.setName("类二分类9");
-			entity.setId(8);
-			result.add(entity);
-			System.out.println(result);
-			return result;
-		}
+		
+//		if (parentId == 0) {
+//			List<CatagoryEntity> result = new ArrayList<CatagoryEntity>();
+//			CatagoryEntity entity = createDefaultCatagoryEntity();
+//			entity.setName("测试种类一");
+//			entity.setId(1);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("测试种类二");
+//			entity.setId(2);
+//			result.add(entity);
+//			System.out.println(result);
+//			return result;
+//		}
+//		if (parentId == 1) {
+//			List<CatagoryEntity> result = new ArrayList<CatagoryEntity>();
+//			CatagoryEntity entity = createDefaultCatagoryEntity();
+//			entity.setName("类一分类一");
+//			entity.setId(3);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类一分类二");
+//			entity.setId(4);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类一分类三");
+//			entity.setId(5);
+//			result.add(entity);
+//			System.out.println(result);
+//			return result;
+//		}
+//		if (parentId == 2) {
+//			List<CatagoryEntity> result = new ArrayList<CatagoryEntity>();
+//			CatagoryEntity entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类1");
+//			entity.setId(6);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类2");
+//			entity.setId(7);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类3");
+//			entity.setId(8);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类4");
+//			entity.setId(9);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类5");
+//			entity.setId(10);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类6");
+//			entity.setId(11);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类7");
+//			entity.setId(12);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类8");
+//			entity.setId(13);
+//			result.add(entity);
+//			entity = createDefaultCatagoryEntity();
+//			entity.setName("类二分类9");
+//			entity.setId(14);
+//			result.add(entity);
+//			System.out.println(result);
+//			return result;
+//		}
 		return processAction("query", parentId);
 	}
 
