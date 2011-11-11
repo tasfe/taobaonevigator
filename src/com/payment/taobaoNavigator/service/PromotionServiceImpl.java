38b6a57b78ca80f668b2cd9df003f74fc7ef10f0
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-11 02:29:57 +0000

    start transaction from service layer

--- src/com/payment/taobaoNavigator/service/PromotionServiceImpl.java
+++ src/com/payment/taobaoNavigator/service/PromotionServiceImpl.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.payment.taobaoNavigator.dao.PromotionDao;
 import com.payment.taobaoNavigator.entity.PromotionEntity;
 
+@Transactional
 @Service
 public class PromotionServiceImpl implements PromotionService {
 	
