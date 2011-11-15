55500f72f617beeb1b9d61071bb4612e0cfc456c
feng.johnson.li@gmail.com <feng.johnson.li@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-15 09:05:33 +0000

    add new method for catagory hot products

--- src/com/payment/taobaoNavigator/service/NavigatorService.java
+++ src/com/payment/taobaoNavigator/service/NavigatorService.java
@@ -1,12 +1,15 @@
 package com.payment.taobaoNavigator.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.payment.taobaoNavigator.entity.CatagoryEntity;
+import com.payment.taobaoNavigator.entity.ProductEntity;
 
 
 public interface NavigatorService {
 	public List<CatagoryEntity> findCatagoryByPid(int pid);
+	public Map<CatagoryEntity, List<ProductEntity>> getHotProducts(int parentId,int maxNumber);
 }
