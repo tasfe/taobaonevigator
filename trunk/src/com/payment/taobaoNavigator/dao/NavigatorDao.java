7c85b6c6b65d92de9a15ccf7ce03721684246bbf
feng.johnson.li@gmail.com <feng.johnson.li@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-15 09:07:24 +0000

    add : 1.add L2 cache for the JPA 2.override the method for maxnum of the result

--- trunk/src/com/payment/taobaoNavigator/dao/NavigatorDao.java
+++ trunk/src/com/payment/taobaoNavigator/dao/NavigatorDao.java
@@ -1,12 +1,22 @@
 package com.payment.taobaoNavigator.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
 import com.payment.taobaoNavigator.entity.CatagoryEntity;
+import com.payment.taobaoNavigator.entity.ProductEntity;
 
 @Repository
 public interface NavigatorDao {
 	public List<CatagoryEntity> findNavigatorByParentId(int pid);
+	public CatagoryEntity findNavigatorById(int id);
+	public CatagoryEntity createNavigator(CatagoryEntity entity);
+	public CatagoryEntity updateNavigator(CatagoryEntity entity);
+	public void deleteNavigator(CatagoryEntity entity);
+	
+	public Map<CatagoryEntity ,List<ProductEntity>>getHotProducts(int pid,int maxNumber);
+	public List<ProductEntity> getHotProduct(String catagoryId,int maxNumber);
+	public List<CatagoryEntity> getAllChildCatagory(int i);
 }
