e8847ba6d8b2753b7da29a2b1f523360f2eef6fe
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-17 08:43:39 +0000

    move database configuration to properties file

---
+++ trunk/src/com/payment/taobaoNavigator/constant/GlobalConstants.java
@@ -0,0 +1,11 @@
+package com.payment.taobaoNavigator.constant;
+
+public class GlobalConstants {
+	
+	//====================DataSource=====================
+	public static final String DB_CONFIG_PATH = "/dataSource.properties";
+	public static final String DB_DRIVER = "driverClassName";
+	public static final String DB_URL = "url";
+	public static final String DB_USER = "username";
+	public static final String DB_PWD = "password";
+}
