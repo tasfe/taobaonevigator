22f735ebb8a6e50fd4704965e7cac196bb6d932b
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-17 07:56:18 +0000

    add //TODO add javadoc

--- src/com/payment/taobaoNavigator/service/PromotionService.java
+++ src/com/payment/taobaoNavigator/service/PromotionService.java
@@ -9,26 +9,37 @@ import com.payment.taobaoNavigator.entity.PromotionEntity;
 @Service
 public interface PromotionService {
 
+	//TODO add javadoc
 	public List<PromotionEntity> getPromotions();
 
+	//TODO add javadoc
 	public List<PromotionEntity> getEnabledPromotions();
-
+	
+	//TODO add javadoc
 	public PromotionEntity getPromotionById(Integer id);
 
+	//TODO add javadoc
 	public PromotionEntity getPromotionByName(String name);
 
+	//TODO add javadoc
 	public PromotionEntity createPromotion(PromotionEntity promotion);
-
+	
+	//TODO add javadoc
 	public PromotionEntity updatePromotion(PromotionEntity promotion);
 
+	//TODO add javadoc
 	public boolean deletePromotionById(Integer id);
 
+	//TODO add javadoc
 	public boolean deletePromotionByIds(List<Integer> ids);
 
+	//TODO add javadoc
 	public boolean deletePromotionByName(String name);
 
+	//TODO add javadoc
 	public boolean deletePromotionByNames(List<String> names);
 
+	//TODO add javadoc
 	public boolean clearPromotions();
 
 }
