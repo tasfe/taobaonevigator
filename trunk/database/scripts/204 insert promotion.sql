95d6d231ff2bb68a2ab1172f87f93fd6d4d95961
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-17 06:54:57 +0000

    number the sql scripts and add insert

---
+++ trunk/database/scripts/204 insert promotion.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `promotion` (`id`, `name`, `description`, `picture`, `catagory_id`, `enabled`) VALUES (1, '11-11 discount for electronics', '11-11 discount 我了个去', 'discount/electronics/1111', 1, 1);
+INSERT INTO `promotion` (`id`, `name`, `description`, `picture`, `catagory_id`, `enabled`) VALUES (2, '11-11 discount for book', '50% OFF', 'discount/book/1111', 4, 1);
+INSERT INTO `promotion` (`id`, `name`, `description`, `picture`, `product_id`, `enabled`) VALUES (3, '11-11 discount for only', 'ONLY clothes promotion', 'discount/clothes/brand/only', 5, 1);
