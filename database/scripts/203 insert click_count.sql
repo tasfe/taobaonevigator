d6f347956ec27ef726665d26accc8a80417ebdc1
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-17 06:54:57 +0000

    number the sql scripts and add insert

---
+++ database/scripts/203 insert click_count.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `click_count` (`id`, `product_id`, `count`, `first_click_date`) VALUES (1, 1, 1000, '2011-11-11');
+INSERT INTO `click_count` (`id`, `product_id`, `count`, `first_click_date`) VALUES (2, 2, 500, '2011-11-11');
+INSERT INTO `click_count` (`id`, `product_id`, `count`, `first_click_date`) VALUES (3, 3, 2000, '2011-11-12');
+INSERT INTO `click_count` (`id`, `product_id`, `count`, `first_click_date`) VALUES (4, 4, 200, '2011-11-13');
