2782c254e41753d3ad9ee17fc4eb5387dcc0e614
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-17 06:59:09 +0000

    comment the commands

--- test/com/payment/taobaoNavigator/data/PromotionDaoImplTest/testGetPromotions.sql
+++ test/com/payment/taobaoNavigator/data/PromotionDaoImplTest/testGetPromotions.sql
@@ -1,9 +1,9 @@
-DELETE FROM `promotion`;
-INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (1,'1','1','1',NULL,NULL,1);
-INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (2,'2','2','2',NULL,NULL,1);
-INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (3,'3','3','3',NULL,NULL,1);
-INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (4,'4','4','4',NULL,NULL,0);
-INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (5,'5','5','5',NULL,NULL,1);
-INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (6,'6','6','6',NULL,NULL,1);
-INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (7,'7','7','7',NULL,NULL,0);
-INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (8,'8','8','8',NULL,NULL,0);
+--DELETE FROM `promotion`;
+--INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (1,'1','1','1',NULL,NULL,1);
+--INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (2,'2','2','2',NULL,NULL,1);
+--INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (3,'3','3','3',NULL,NULL,1);
+--INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (4,'4','4','4',NULL,NULL,0);
+--INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (5,'5','5','5',NULL,NULL,1);
+--INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (6,'6','6','6',NULL,NULL,1);
+--INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (7,'7','7','7',NULL,NULL,0);
+--INSERT INTO `promotion` (`id`,`name`,`description`,`picture`,`product_id`,`catagory_id`,`enabled`) VALUES (8,'8','8','8',NULL,NULL,0);
