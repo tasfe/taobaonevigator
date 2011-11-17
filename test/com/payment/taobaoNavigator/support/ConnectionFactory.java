72a80c5a77b778fd97e261311056bdfc91bfac9d
dxp4598@gmail.com <dxp4598@gmail.com@32acca57-b503-4b64-e23f-3b7ee0d234ce>
2011-11-17 08:43:19 +0000

    move database configuration to properties file

--- test/com/payment/taobaoNavigator/support/ConnectionFactory.java
+++ test/com/payment/taobaoNavigator/support/ConnectionFactory.java
@@ -1,57 +1,45 @@
 package com.payment.taobaoNavigator.support;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
+
+import com.payment.taobaoNavigator.constant.GlobalConstants;
 
 public final class ConnectionFactory {
 
 	public static final Logger logger = Logger.getLogger(ConnectionFactory.class);
+
+	private static final Properties props = loadDatabaseProps();
 	
-	//TODO put these information into properties
-	private static final String DATABASE_URL = "jdbc:mysql://192.168.71.187:3306/taobaonavigator_test?useUnicode=true&characterEncoding=UTF-8";
-	private static final String DATABASE_USER = "test";
-	private static final String DATABASE_PASSWORD = "000000";
-	private static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+	private static Properties loadDatabaseProps() {
+		Properties props = new Properties();
+        try {
+        	InputStream in = ConnectionFactory.class.getResourceAsStream(GlobalConstants.DB_CONFIG_PATH);
+        	props.load(in);	
+        } catch (IOException e) {
+        	logger.error("ERROR:could not load properties for database initialization.", e);
+        }
+        return props;
+	}
 	
 	private ConnectionFactory() {
 		try {
-			Class.forName(DATABASE_DRIVER);
+			Class.forName(GlobalConstants.DB_DRIVER);
 		} catch (ClassNotFoundException e) {
-			logger.error("ERROR:exception loading driver class");
+			logger.error("ERROR:exception loading driver class", e);
 		}
 	}
 
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
-
-	}
-
-	public static void close(ResultSet rs) {
-		try {
-			rs.close();
-		} catch (Exception ignored) {
-			
-		}
-	}
-
-	public static void close(Statement stmt) {
-		try {
-			stmt.close();
-		} catch (Exception ignored) {
-			
-		}
-	}
-
-	public static void close(Connection conn) {
-		try {
-			conn.close();
-		} catch (Exception ignored) {
-			
-		}
+		String url = props.getProperty(GlobalConstants.DB_URL);
+		String username = props.getProperty(GlobalConstants.DB_USER);
+		String password =props.getProperty(GlobalConstants.DB_PWD);
+		return DriverManager.getConnection(url, username, password);
 	}
 }
